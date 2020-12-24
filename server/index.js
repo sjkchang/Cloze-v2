c9597bb941a83e89667e6a3922991004741567e9
sjkchang <sjkchang@gmail.com>
2020-12-23 21:15:54 -0800

    Connected server with MongoDB Atlas cluster

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
-const app = express();
-const port = 5000;
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const userRoutes = require('./routes/user-routes.js');
-const todoRoutes = require('./routes/todo-routes.js');
-const journalRoutes = require('./routes/journal-routes.js');
-const challengeRoutes = require('./routes/challenge-routes.js');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const userRoutes = require('./routes/users.js');
+const todoRoutes = require('./routes/todos.js');
+const journalRoutes = require('./routes/journals.js');
+const challengeRoutes = require('./routes/challenges.js');
+
+
+
+
+
+app.use(cors);
+app.use(express.json());
 
 app.use('/user', userRoutes);
 app.use('/to-do', todoRoutes);
 app.use('/journal', journalRoutes);
 app.use('/challenge', challengeRoutes);
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => console.log("MongoDB database connection established"))
 
 app.listen(port, () => console.log(`Cloze V2 is running at http://localhost:${port}`));
