cab71f13dad57577b5576b92a0b13622135c4d30
sjkchang <sjkchang@gmail.com>
2020-12-29 20:35:25 -0800

    Moved database connection logic to index.js from mongo_db.js. Deleted mongo_db.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,33 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect(uri,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+app.use(express.json());
+
 
 const userRoutes = require('./routes/users.js');
 const todoRoutes = require('./routes/todos.js');
 const journalRoutes = require('./routes/journals.js');
 const challengeRoutes = require('./routes/challenges.js');
 
-
-
-//app.use(cors);
-//app.use(express.json());
 
 app.use('/user', userRoutes);
 app.use('/to-do', todoRoutes);
