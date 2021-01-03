4e59ded8886be53897afcb57ba5b1cc864bdb589
sjkchang <sjkchang@gmail.com>
2021-01-02 19:16:25 -0800

    Simplified journal routing

---
+++ server/controllers/journalController.js
@@ -0,0 +1,25 @@
+const Entry = require('../models/entryModel')
+
+exports.list_all_entries = (req, res) => {
+    res.send('This route is not yet implemented')
+}
+
+exports.list_all_users_entries = (req, res) => {
+    res.send('This route is not yet implemented')
+}
+
+exports.get_entry = (req, res) => {
+    res.send('This route is not yet implemented')
+}
+
+exports.add_entry = (req, res) => {
+    res.send('This route is not yet implemented')
+}
+
+exports.edit_entry = (req, res) => {
+    res.send('This route is not yet implemented')
+}
+
+exports.delete_entry = (req, res) => {
+    res.send('This route is not yet implemented')
+}
--- server/routes/journals.js
+++ server/routes/journals.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const journal_controller = require('../controllers/journalController');
 
-router.get('/', (req, res) => res.send('GET route on journal routes.'));
 
-router.post('/', (req, res) => res.send('POST route on journal routes.'));
+//Get response of all users
+router.get('/', journal_controller.list_all_users);
+
+router.route('/:title')
+    .get(journal_controller.get_user)
+    .post(journal_controller.register_new_user)
+    .put(journal_controller.edit_user)
+    .delete(journal_controller.delete_user);
 
 //export this router to use in our index.js
 module.exports = router;
