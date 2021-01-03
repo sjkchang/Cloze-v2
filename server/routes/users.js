9f8992f96ee9f39797999b5484f5d73099c1831b
sjkchang <sjkchang@gmail.com>
2021-01-02 19:15:21 -0800

    simplified user routing

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,20 +1,16 @@
 const router = require('express').Router();
-
-//Import controllers
 const user_controller = require('../controllers/userController');
+
 
 //Get response of all users
 router.get('/', user_controller.list_all_users);
 
-//Add a new user
-router.post('/add', user_controller.register_new_user);
-
 //Get JSON data on specific user based off of username
-router.get('/:username', user_controller.get_user);
+router.route('/:username')
+    .get(user_controller.get_user)
+    .post(user_controller.register_new_user)
+    .put(user_controller.edit_user)
+    .delete(user_controller.delete_user);
 
-//Edit User
-router.put('/edit', user_controller.edit_user);
-
-router.delete('/:username', user_controller.delete_user);
 //export this router to use in our index.js
 module.exports = router;
