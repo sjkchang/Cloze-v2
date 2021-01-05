6a9ecc978889c120e7f760d2de5db2d7d7992611
sjkchang <sjkchang@gmail.com>
2021-01-04 20:27:12 -0800

    Added type verification to post model so only valid types are accepted

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -4,8 +4,20 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     title: { type: String, required: true, trim: true},
     content: { type: String, required: true },
-    type: { type: String, required: true},
-    user: {type: mongoose.Schema.Types.ObjectID, ref: 'User'}
+    type: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: (str) => {
+                return (str == 'Entry' || str == 'Task' || str == 'Challenge')
+            },
+            message: 'Post type must be either Entry, Task or Challenge'
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectID, 
+        ref: 'User',
+    }
 },{
     timestamps: true
 });
