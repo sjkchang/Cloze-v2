0d08aa27457ea77dc93d9ea3d65631c0b12cb69f
sjkchang <sjkchang@gmail.com>
2021-01-10 17:50:38 -0800

    Simplified user controller, removing async code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,29 +1,27 @@
 const User = require('../models/userModel')
 
 exports.list_all_users = (req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(error => res.status(400).json('Error: ' + error))
+    User.find((err, users) => {
+        if(err){ res.status(400).json('Error: ' + error)}
+        res.json(users);
+    });
 }
 
 exports.get_user = (req, res) => {
     const username = req.params.username;
-    User.find({ username: username })
-        .then(user => res.json(user))
-        .catch(error => res.status(400).json('Error: ' + error));
+    User.find({username: username}, (err, user) => {
+        if (err) { res.status(400).json('Error: ' + error) }
+        res.json(user);
+    });
 }
 
 exports.register_new_user = (req, res) => {
     const username = req.body.username;
     const name = req.body.name;
-    const newUser = new User({username: username, name: name});
-
-    newUser.save()
-        .then(() => res.json('User ' + username + ' added!'))
-        .catch(error => {
-            res.status(400).json('Error: ' + error);
-            console.log(error);
-        })
+    const newUser = User.create({username: username, name: name}, (err, user) => {
+        if(err) { res.status(400).json('Error: ' + err); }
+        res.json(user);
+    });
 }
 
 exports.edit_user = (req, res) => {
@@ -31,26 +29,18 @@ exports.edit_user = (req, res) => {
     const name = req.body.name;
     const old_username = req.body.old_username;
 
-    User.findOneAndUpdate({username: old_username}, {$set: {username: username, name: name}})
-        .then((user) => {
-            if(user) {
-                res.json('User' + old_username + ' has been updated to ' + username);
-            } else {
-                throw error = new Error('User does not Exist');
-            }
-        }).catch(error => {
-            res.status(400).json('Error: ' + error);
-            console.log(error);
-        });
-    
+    User.findOneAndUpdate({username: old_username}, {$set: {username: username, name: name}}, (err, user) => {
+        if(err){res.status(400).json('Error: ' + error);}
+        res.json('User' + user.username + ' has been updated to ' + username);
+    })
 }
 
 exports.delete_user = (req, res) => {
     const username = req.params.username;
-    User.findOneAndDelete({username: username})
-        .then(() => res.json('User ' + username + ' deleted!'))
-        .catch(error => {
-            res.status(400).json('Error: ' + error);
-            console.log(error);
-        })
+    User.findOneAndDelete({username: username}, (err, user) => {
+        if(err){
+            res.status(400).json('Error: ' + err);
+        }
+        res.json('User ' + username + ' deleted!');
+    });
 }
